5ea258bfd37500f71fb28f1a8243e5aaac4469ba
Suman Pradhab <11.sumanpradhan@gmail.com>
2020-11-23 14:25:12 -0600

    Updated all the logic for controller methods

--- src/main/java/com/pradhan/ebanking/eBanking_Backend/service/AccountService.java
+++ src/main/java/com/pradhan/ebanking/eBanking_Backend/service/AccountService.java
@@ -1,2 +1,139 @@
-package com.pradhan.ebanking.eBanking_Backend.service;public class AccountService {
+package com.pradhan.ebanking.eBanking_Backend.service;
+
+import com.pradhan.ebanking.eBanking_Backend.beans.*;
+import com.pradhan.ebanking.eBanking_Backend.repository.AccountRepository;
+import com.pradhan.ebanking.eBanking_Backend.repository.CheckingAccountRepository;
+import com.pradhan.ebanking.eBanking_Backend.repository.SavingsAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    SavingsAccountRepository savingsRepository;
+
+    @Autowired
+    CheckingAccountRepository checkingRepository;
+
+
+
+    public Account registerNewAccount(Customer customer) {
+
+        int low = 111111;
+        int limit = 999999;
+        Random rand =  new Random();
+        int rand_acct_num =  rand.nextInt(limit-low) + low;
+        Account account =  new Account(rand_acct_num, customer);
+        System.out.println(account.toString());
+        System.out.println(customer.toString());
+        accountRepository.save(account);
+        return account;
+    }
+
+    public void registerCheckingAndSavingsAccount(Account account) {
+        // 10 is the starting amount
+        Savings savings = new Savings(new BigDecimal(10),account);
+        Checking checking =  new Checking(new BigDecimal(10), account);
+        savingsRepository.save(savings);
+        checkingRepository.save(checking);
+    }
+
+
+    public AccountDetails getAccountDetails(Account account) {
+        Savings custSavings  =  savingsRepository.findByAccountId(account.getId());
+        Checking  custChecking =  checkingRepository.findByAccountId(account.getId());
+
+        AccountDetails accountDetails = new AccountDetails();
+        accountDetails.setCheckingId(custChecking.getId());
+        accountDetails.setCheckingBalance(custChecking.getBalance());
+        accountDetails.setSavingsId(custSavings.getId());
+        accountDetails.setSavingsBalance(custSavings.getBalance());
+
+        return accountDetails;
+    }
+
+    public Savings getSavingsInfo(int accountId) {
+        //TODO: Change type of Account.accountId to long
+        return accountRepository.findAccountByAccountId(accountId).getSavings();
+    }
+
+
+    public boolean depositSavings(long amount, long savingsId) throws Exception {
+        if (Long.valueOf(savingsId) != null & savingsId <= 0) {
+            if (isValidSavingsId(savingsId)) {
+                Savings savings = savingsRepository.findById(savingsId).get();
+                BigDecimal oldAmount = savings.getBalance();
+                BigDecimal depositAmount = new BigDecimal(amount);
+                BigDecimal newAmount = oldAmount.add(depositAmount);
+                savings.setBalance(newAmount);
+                savingsRepository.save(savings);
+                return true;
+            } else
+                throw new Exception("Unable to find Savings invalid Id");
+        } else {
+            throw new NullPointerException("Savings Id cannot be null or less or equal to 0");
+        }
+
+    }
+
+    public boolean withdrawSavings(long amount, long savingsId) throws Exception {
+        if (Long.valueOf(savingsId) != null & savingsId <= 0) {
+            if (isValidSavingsId(savingsId)) {
+                Savings savings = savingsRepository.findById(savingsId).get();
+                if (amount <= savings.getBalance().longValue()) {
+                    BigDecimal withdrawAmount = new BigDecimal(amount);
+                    BigDecimal newAmount = savings.getBalance().subtract(withdrawAmount);
+                    savings.setBalance(newAmount);
+                    savingsRepository.save(savings);
+                    return true;
+                } else {
+                    throw new ArithmeticException("Withdraw amount is greater than remaining balance.");
+                }
+            }
+            else
+                throw new Exception("Unable to find Savings invalid Id");
+        }
+        else {
+            throw new NullPointerException("Savings Id cannot be null or less or equal to 0");
+        }
+    }
+
+
+    public boolean isValidSavingsId( long savingsId) {
+
+        List<Account> accountList =  accountRepository.findAll();
+        List<Long> savingsIdList = new ArrayList<>();
+        accountList.forEach((account -> {
+            savingsIdList.add(account.getSavings().getId());
+        }));
+        if (savingsIdList.contains(savingsId)) {
+            return true;
+        }
+        else return false;
+    }
+
+
+    public void transferToChecking(Account account, long amount) {
+        Savings savings =  account.getSavings();
+        Checking checking = account.getChecking();
+        savings.setBalance(savings.getBalance().subtract(new BigDecimal(amount)));
+        // TODO: Why is it everytime null
+        if (checking.getBalance() == null) {
+            checking.setBalance(new BigDecimal(amount));
+        } else {
+            checking.setBalance(checking.getBalance().add(new BigDecimal(amount)));
+        }
+        savingsRepository.save(savings);
+        checkingRepository.save(checking);
+    }
 }
--- src/main/java/com/pradhan/ebanking/eBanking_Backend/service/CustomerService.java
+++ src/main/java/com/pradhan/ebanking/eBanking_Backend/service/CustomerService.java
@@ -1,2 +1,75 @@
-package com.pradhan.ebanking.eBanking_Backend.service;public class CustomerService {
+package com.pradhan.ebanking.eBanking_Backend.service;
+
+import com.pradhan.ebanking.eBanking_Backend.beans.Account;
+import com.pradhan.ebanking.eBanking_Backend.beans.Customer;
+import com.pradhan.ebanking.eBanking_Backend.dto.CustomerDto;
+import com.pradhan.ebanking.eBanking_Backend.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+    public Customer loginUser(CustomerDto customerDto){
+        if(!customerDto.getUserName().isEmpty() && !customerDto.getPassword().isEmpty()) {
+            return this.customerRepository.findByUserName(customerDto.getUserName());
+        } else {
+            throw new IllegalArgumentException("Username and password invalid");
+        }
+    }
+
+    public Customer registerUser(CustomerDto customerDto) {
+        //TODO: Check if username or email already exists
+        //if(customerRepository.findByEmail(customerDto.getEmail() ))
+        Customer customer = createCustomer(customerDto);
+        customerRepository.save(customer);
+        return customer;
+
+    }
+
+    private Customer createCustomer(CustomerDto customerDto) {
+        return new Customer(
+                customerDto.getFirstName(), customerDto.getLastName(),
+                customerDto.getUserName(), customerDto.getPassword(),
+                customerDto.getEmail(), customerDto.getAddress(),
+                customerDto.getPhoneNumber()
+        );
+    }
+
+
+    public Account getUserAccount(String userName) throws Exception{
+        if (isValidUser(userName)) {
+            return customerRepository.findByUserName(userName).getUserAccount();
+        } else {
+            throw new Exception("Username: " + userName+ " does not exists!");
+        }
+    }
+
+    public boolean isValidUser(String userName) {
+        List<Customer> customerList =  this.customerRepository.findAll();
+        List<String> users = new ArrayList();
+        customerList.forEach(customer -> {
+            users.add(customer.getUserName());
+        });
+        if(!userName.isEmpty() && users.contains(userName)) {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    public Customer findByUserName(String userName) throws Exception {
+        if (isValidUser(userName)) {
+            return  customerRepository.findByUserName(userName);
+        } else
+            throw new Exception("Username: "+ userName+ " does not exists");
+    }
 }
