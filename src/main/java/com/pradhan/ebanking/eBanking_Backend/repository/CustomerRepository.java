3c74b11ff5c4a55dbc49fa1d7ae4c926a78a1dd1
Suman Pradhab <11.sumanpradhan@gmail.com>
2020-12-02 14:41:50 -0600

    Created queries to return list of email and list of phone number

--- src/main/java/com/pradhan/ebanking/eBanking_Backend/repository/CustomerRepository.java
+++ src/main/java/com/pradhan/ebanking/eBanking_Backend/repository/CustomerRepository.java
@@ -3,10 +3,21 @@ package com.pradhan.ebanking.eBanking_Backend.repository;
 
 import com.pradhan.ebanking.eBanking_Backend.beans.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     Customer findByUserName(String username);
+
+
+    @Query("select email from Customer")
+    List<String> findAllEmail();
+
+    @Query("select phoneNumber from Customer")
+    List<String> findAllPhoneNumber();
+
 }
