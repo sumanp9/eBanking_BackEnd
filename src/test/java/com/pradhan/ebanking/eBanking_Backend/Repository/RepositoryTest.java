02837ef080e0baf54c4d9a6b416496c7682bda57
Suman Pradhab <11.sumanpradhan@gmail.com>
2020-12-02 14:40:13 -0600

    Created queries to return list of email and list of phone number

--- src/test/java/com/pradhan/ebanking/eBanking_Backend/Repository/RepositoryTest.java
+++ src/test/java/com/pradhan/ebanking/eBanking_Backend/Repository/RepositoryTest.java
@@ -52,5 +52,23 @@ public class RepositoryTest {
         Assert.assertNotEquals(customer, unExpected);
 
     }
+/*
+    @Test
+    public void validUserTest() {
+    /*
+    Boolean cannot be returned by findAll()
+    findAll() should return List
+    ***
 
+    If you're unsure why you're getting above error read on.
+    Due to the nature of the syntax above problem might occur because:
+        1. This exception *might* occur in wrongly written multi-threaded tests.
+         Please refer to Mockito FAQ on limitations of concurrency testing.
+        2. A spy is stubbed using when(spy.foo()).then() syntax. It is safer to stub spies -
+       - with doReturn|Throw() family of methods. More in javadocs for Mockito.spy() method.
+
+
+        given(this.customerService.isValidUser("thanos")).willReturn(true);
+    }
+*/
 }
