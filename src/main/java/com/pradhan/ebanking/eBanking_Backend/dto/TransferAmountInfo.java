649f078ae44cdee77f3b1b52eb4f40764d6aa5aa
Suman Pradhab <11.sumanpradhan@gmail.com>
2020-12-15 18:17:54 -0600

    updated Class to contain date

--- src/main/java/com/pradhan/ebanking/eBanking_Backend/dto/TransferAmountInfo.java
+++ src/main/java/com/pradhan/ebanking/eBanking_Backend/dto/TransferAmountInfo.java
@@ -1,16 +1,24 @@
 package com.pradhan.ebanking.eBanking_Backend.dto;
 
+import java.util.Date;
+
 public class TransferAmountInfo {
 
     private String senderUserName;
-    private int otherAccountNumber;
+    private int otherAcctNumber;
+    private String accountType;
+    private Date date;
 
 
     public String getSenderUserName() {
         return senderUserName;
     }
 
-    public int getOtherAccountNumber() {
-        return otherAccountNumber;
+    public int getOtherAcctNumber() {
+        return otherAcctNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
     }
 }
